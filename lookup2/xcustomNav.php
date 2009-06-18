4cf7d7a732705133203aa040a4467b2b711bd8a5
Fred LaPlante <flaplante@flos-inc.com>
2009-06-17 22:58:40 -0400

    remove old deadwood files

--- lookup2/xcustomNav.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($nav == "lookup") { ?>
- &raquo; <?php echo $navLoc->getText("lookup");?><br>
-<?php } else { ?>
- <a href="../lookup2/lookup.php" class="alt1"><?php echo $navLoc->getText("lookup");?></a><br>
-<?php
-}
-?>
