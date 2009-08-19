3cc819ccef39e98ba6f387b622105d7538b2461b
Fred LaPlante <flaplante@flos-inc.com>
2009-08-19 14:05:20 -0400

    mbr_hist: minor changes for consistenet appearance;

--- circ/mbr_account.php
+++ circ/mbr_account.php
@@ -11,7 +11,6 @@
 	$focus_form_field = "transactionTypeCd";
 
 	require_once(REL(__FILE__, "../functions/inputFuncs.php"));
-//	require_once(REL(__FILE__, "../functions/formatFuncs.php"));
 	require_once(REL(__FILE__, "../shared/logincheck.php"));
 	require_once(REL(__FILE__, "../shared/get_form_vars.php"));
 	require_once(REL(__FILE__, "../model/MemberAccounts.php"));
