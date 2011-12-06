5e5d64ff6e75ecca1798ef59816854e87925ba29
Fred LaPlante <flaplante@flos-inc.com>
2011-12-06 17:14:27 -0500

    2nd draft of CSV file output;

--- layouts/default/csv.php
+++ layouts/default/csv.php
@@ -7,8 +7,6 @@ require_once(REL(__FILE__, '../../model/Biblios.php'));
 
 class Layout_csv {
 	function render($rpt) {
-//		header('Content-Type: application/text');
-//		header('Content-disposition: inline; filename="export.txt"');
 		$biblios = new Biblios;
 		$tags = array();
 		$collection = array();
@@ -22,45 +20,51 @@ class Layout_csv {
 
 			$item = array();
 			foreach ($flds as $fld) {
-//var_dump($fld);echo "\r\n";
 				$tag = $fld->tag;
 				$sub = $fld->subfields[0]->identifier;
-				$val = $fld->subfields[0]->data;
-				$marcId = $tag.$sub;
 				## construct a list of all tags used
-				if (! in_array($marcId, $tags)) $tags[] = $marcId;
-				## save information for easy processing
-				if (! array_key_exists($marcId, $item)) $item[$marcId] = $val;
-				//echo "$tag$sub ==>> $val<br />\r\n";
-//echo "<br />. . . . . . .<br />\r\n";
+				if (($tag >= 100) && ($tag < 300)) {
+					$marcId = $tag.$sub;
+					if (!in_array($marcId, $tags)) {
+						$tags[] = $marcId;
+					}
+
+					## save information for easy processing later
+					if (! array_key_exists($marcId, $item)) {
+						$item[$marcId] = $fld->subfields[0]->data;
+					}
+				}
 			}
 			ksort($item);
 			$collection[] = $item;
-//exit;
 		}
-		sort($tags);
+		natsort($tags);
 		
+		echo "<pre>";
 		## first we print the column headings
-		//print_r($tags);;echo "<br />\r\n";
+		$outStr = '';
 		foreach ($tags as $tag){
-			$t = substr($tag,0,3);
-			if (($t >= 100) && ($t < 300)) echo "$tag; ";
+			$outStr .= "$tag,";
 		}
-		echo "<br />\r\n-----------------------------------------<br />\r\n";
+		$out = substr($outStr,0,-1); // remove trailing chars
+		echo $out."\n";
 		
 		## then we print the citation string
 		foreach ($collection as $item) {
-			//print_r($item);echo "<br />\r\n";
-			foreach ($item as $key=>$val){
-				$tag = substr($key,0,3);
-				if (($tag >= 100) && ($tag < 300)) {
-					if ($tag == 245) $val = '"'.$val.'"';
-					echo "$val; ";
-				}
+			$outStr = '';
+			foreach ($tags as $tag){
+				if (array_key_exists($tag, $item))
+					$val = trim($item[$tag]);
+				else
+					$val = "";
+				$val = trim(preg_replace('/[,:;.\s]$/',"",$val));	// trailing char
+				$val = preg_replace('/\'\'/',"'",$val); // embeded quotes
+				$val = '"'.$val.'"';
+				$outStr .= "$val,";
 			}
-			echo "<br />\r\n. . . . . . .<br />\r\n";
+			$out = substr($outStr,0,-1); // remove trailing chars
+			echo $out."\n";
 		}
-		echo "-----------------------------------------<br />\r\n";
-		//print_r($collection);
+		echo "</pre>";
 	}
 }
