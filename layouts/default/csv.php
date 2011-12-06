a8b8017bceaf6d4bac63798a03918de2c07a9b66
Fred LaPlante <flaplante@flos-inc.com>
2011-12-06 11:08:08 -0500

    next draft of CSV file output;

--- layouts/default/csv.php
+++ layouts/default/csv.php
@@ -10,28 +10,57 @@ class Layout_csv {
 //		header('Content-Type: application/text');
 //		header('Content-disposition: inline; filename="export.txt"');
 		$biblios = new Biblios;
+		$tags = array();
+		$collection = array();
 		while ($row = $rpt->each()) {
 			$bib = $biblios->getOne($row['bibid']);
 			if (!$bib['marc']) {
 				continue;
 			}
-//var_dump($bib);echo "\r\n";
-//echo ".=.=.=.=.=.=.=.=.=.=.=.\r\n";
 			$marc = $bib['marc'];
-//var_dump($marc);echo "\r\n";
-//echo ".=.=.=.=.=.=.=.=.=.=.=.\r\n";
 			$flds = $marc->fields;
-//var_dump($flds);echo "\r\n";
+
+			$item = array();
 			foreach ($flds as $fld) {
 //var_dump($fld);echo "\r\n";
 				$tag = $fld->tag;
 				$sub = $fld->subfields[0]->identifier;
 				$val = $fld->subfields[0]->data;
-				echo "$tag$sub ==>> $val<br />\r\n";
-//echo ". . . . . . .\r\n";
+				$marcId = $tag.$sub;
+				## construct a list of all tags used
+				if (! in_array($marcId, $tags)) $tags[] = $marcId;
+				## save information for easy processing
+				if (! array_key_exists($marcId, $item)) $item[$marcId] = $val;
+				//echo "$tag$sub ==>> $val<br />\r\n";
+//echo "<br />. . . . . . .<br />\r\n";
 			}
-echo "-----------------------------------------<br />\r\n";
+			ksort($item);
+			$collection[] = $item;
 //exit;
 		}
+		sort($tags);
+		
+		## first we print the column headings
+		//print_r($tags);;echo "<br />\r\n";
+		foreach ($tags as $tag){
+			$t = substr($tag,0,3);
+			if (($t >= 100) && ($t < 300)) echo "$tag; ";
+		}
+		echo "<br />\r\n-----------------------------------------<br />\r\n";
+		
+		## then we print the citation string
+		foreach ($collection as $item) {
+			//print_r($item);echo "<br />\r\n";
+			foreach ($item as $key=>$val){
+				$tag = substr($key,0,3);
+				if (($tag >= 100) && ($tag < 300)) {
+					if ($tag == 245) $val = '"'.$val.'"';
+					echo "$val; ";
+				}
+			}
+			echo "<br />\r\n. . . . . . .<br />\r\n";
+		}
+		echo "-----------------------------------------<br />\r\n";
+		//print_r($collection);
 	}
 }
