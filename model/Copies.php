3ea16d10b15d905303318835a9d8cffe51d93161
Fred LaPlante <flaplante@flos-inc.com>
2016-05-30 19:16:43 -0400

    New Copy and New Photo now working on New Items;

--- model/Copies.php
+++ model/Copies.php
@@ -85,13 +85,13 @@ class Copies extends CoreTable {
 		//$sql = $this->mkSQL("select max(copyid) as lastCopy from biblio_copy");
 		$sql = $this->mkSQL("select max(barcode_nmbr) as lastNmbr from biblio_copy");
 		$cpy = $this->select1($sql);
-//echo "in Copies::getNewBarCode(): ";print_r($cpy);echo "<br />\n";
+        //echo "in Copies::getNewBarCode(): ";print_r($cpy);echo "<br />\n";
         //$nmbr = $cpy[lastNmbr]+1;
         //$nmbr = $cpy['barcdNmbr']+1;
-        if (strpos($cpy, "error"))
-            $nmbr = 1;
-        else
-            $nmbr = $cpy['barcdNmbr']+1;
+        //if (strpos($cpy, "error"))
+        //    $nmbr = 1;
+        //else
+            $nmbr = $cpy[0]['lastNmbr']+1;
 	    if(empty($width)) $w = 13; else $w = $width;
 		return sprintf("%0".$w."s",$nmbr);
 	}
