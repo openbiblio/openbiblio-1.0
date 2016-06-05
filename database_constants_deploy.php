b5386878620f3f7a836e629f5940a34b4e1e3fc8
Celso Corona <celso_corona@yahoo.com>
2016-06-05 12:11:01 -0300

    actualiza

--- database_constants_deploy.php
+++ database_constants_deploy.php
@@ -1,18 +1,6 @@
-<?php
-/*********************************************************************************
- *
- *                           A T T E N T I O N !
- *
- *  ||  Please modify the following database connection variables to match  ||
- *  \/  the MySQL database and user that you have created for OpenBiblio.   \/
- *********************************************************************************
- */
-define("OBIB_HOST",     "localhost");
-define("OBIB_DATABASE", "OpenBiblio");
-define("OBIB_USERNAME", "usuario-mysql con todos los derechos sobre la base de datos");
-define("OBIB_PWD",      "contraseña del usuario-mysql");
-/*********************************************************************************
- *  /\                                                                      /\
- *  ||                                                                      ||
- *********************************************************************************
- */
+<?php 
+$this->dsn["host"] = 'localhost'; 
+$this->dsn["username"] = 'MY USERNAME'; 
+$this->dsn["pwd"] = 'MY PASSWORD'; 
+$this->dsn["database"] = 'OpenBiblio'; 
+$this->dsn["mode"] = haveConst; 
