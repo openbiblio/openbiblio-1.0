b563ffed9de18b167cae035faaff574db118b01b
Jane Sandberg <sandbej@linnbenton.edu>
2016-07-21 16:11:26 +0000

    fixing sql syntax

--- install/1.0b/sql/member.sql
+++ install/1.0b/sql/member.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS %prfx%.member (
   mbrid int NOT NULL AUTO_INCREMENT,
   barcode_nmbr varchar(20) NOT NULL DEFAULT '',
   create_dt datetime NOT NULL,
-  last_change_dt timestamp ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+  last_change_dt timestamp ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   last_change_userid int NOT NULL DEFAULT '0',
   first_name varchar(50) NOT NULL DEFAULT '',
   last_name varchar(50) NOT NULL DEFAULT '',
