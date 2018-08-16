021a79e025296525d9e3336ce6e410fe2afd9bc2
Jane Sandberg <sandbej@linnbenton.edu>
2018-08-16 14:50:37 -0700

    Adding a self-check plug-in

---
+++ plugin_selfCheck/selfCheckForms.php
@@ -0,0 +1,259 @@
+<?php
+/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
+ * See the file COPYRIGHT.html for more details.
+ */
+
+	require_once("../shared/common.php");
+
+	$tab = "selfCheck";
+	$nav = "selfCheck";
+        $focus_form_name = "barCdSrchForm";
+        $focus_form_field = "searchByBarcd";
+
+	Page::header(array('nav'=>$tab.'/'.$nav, 'title'=>''));
+
+?>
+<h3><?php echo T("Self check"); ?></h3>
+
+<!-- ------------------------------------------------------------------------ -->
+<div id="msgDiv"><fieldSet id="userMsg"></fieldset></div>
+
+<!-- ------------------------------------------------------------------------ -->
+<p id="errSpace" class="error"></p>
+<div id="searchDiv">
+	<form role="form" id="barCdSrchForm" name="barCdSrchForm" action="">
+	<fieldset>
+	<legend><?php echo T("Find Member by Barcode"); ?></legend>
+	<table>
+		<tr>
+			<td nowrap="true">
+				<label for="searchByBarcd"><?php echo T("Library Card Number");?>:</label>
+				<input type="number" id="searchByBarcd" name="searchByBarcd" size="20" />
+				<input type="button" id="barCdSrchBtn" value="<?php echo T("Search"); ?>" />
+			</td>
+		</tr>
+	</table>
+	</fieldset>
+	</form>
+	
+</div>
+
+<!-- ------------------------------------------------------------------------ -->
+<div id="mbrDiv">
+	<input type="button" class="gobkMbrBtn" value="<?php echo T("Go Back"); ?>" />
+	<fieldset id="identity">
+		<legend><?php echo T("Member Information");?></legend>
+		<label for="mbrName"><?php echo T("Name");?>:</label>
+		<input type="text" readonly="readonly" id="mbrName" />
+		<input type="button" value="<?php echo T("Details");?>" id="mbrDetlBtn" />
+		<input type="button" value="<?php echo T("Account");?>" id="mbrAcntBtn" />
+		<input type="button" value="<?php echo T("History");?>" id="mbrHistBtn" />
+		<br />
+		<label for="mbrSite"><?php echo T("Site");?>:</label>
+		<input type="text" readonly id="mbrSite" />
+		<br />
+		<label for="MbrCardNo"><?php echo T("Card Number");?>:</label>
+		<input type="number" readonly id="mbrCardNo" />
+	</fieldset>
+<!-- -->
+	<fieldset id="newLoans">
+		<legend for="ckoutBarcd"><?php echo T("Check Out");?></legend>
+		<label><?php echo T("Barcode Number");?>:</label>
+		<input type="number" id="ckoutBarcd" size="20" min="1" />
+		<input type="button" value="<?php echo T("Check Out");?>" id="chkOutBtn" />
+		<p class="error" id="chkOutMsg"></p>
+	</fieldset>
+<!-- -->
+	<fieldset id="onLoan">
+		<legend><?php echo T("Items Currently Checked Out");?></legend>
+		<table id="chkOutList">
+			<thead>
+			<tr>
+				<th class="center"><?php echo T("Checked Out");?></th>
+				<th class="center"><?php echo T("Media");?></th>
+				<th class="center"><?php echo T("Barcode");?></th>
+				<th class="center"><?php echo T("Title");?></th>
+				<th class="center"><?php echo T("Due Back");?></th>
+				<th class="center"><?php echo T("Days Late");?></th>
+				<th class="center"><?php echo T("AmountOwed");?></th>
+			</tr>
+			</thead>
+			<tbody class="striped"></tbody>
+			<tfoot class="topBorder">
+			<tr>
+				<th><?php echo T("Limit");?></th>
+				<td id="maxFine"></td>
+				<td colspan="3">&nbsp</td>
+        <th><?php echo T("Total");?></th>
+				<td id="ttlOwed" class="number"></td>
+			</tr>
+      </tfoot>
+		</table>
+	</fieldset>
+	<input type="button" class="gobkMbrBtn" value="<?php echo T("Go Back"); ?>" />
+</div>
+
+<!-- ------------------------------------------------------------------------ -->
+<div id="listDiv">
+	<h5><?php echo T("SearchResults"); ?></h5>
+	<div id="results_found">
+		<?php //echo T("biblioSearchMsg", array('nrecs'=>$rpt->count(), 'start'=>1, 'end'=>25)); ?>
+	</div>
+	<table>
+	<thead>
+	<tr>
+		<td><input type="button" class="gobkBtn" value="<?php echo T("Go Back"); ?>" /></td>
+		<td width="80%" align="right">
+			<input type="button" class="goPrevBtn PgBtn" value="<?php echo T("Previous Page"); ?>">
+			<span class="rsltQuan"></span>
+			<input type="button" class="goNextBtn PgBtn" value="<?php echo T("Next Page"); ?>">
+		</td>
+	</tr>
+	</thead>
+	<tbody>
+	<tr>
+	  <td colspan="3">
+			<fieldset>
+				<table id="listTbl" width="100%">
+					<tbody id="srchRslts" class="striped"><tr><td>filled by server</td></tr></tbody>
+				</table>
+			</fieldset>
+		</td>
+	</tr>
+	</tbody>
+	<tfoot>
+	<tr>
+		<td><input type="button" class="gobkBtn" value="<?php echo T("Go Back"); ?>" /></td>
+		<td width="80%" align="right">
+			<input type="button" class="goPrevBtn PgBtn" value="<?php echo T("Previous Page"); ?>">
+			<span class="rsltQuan"></span>
+			<input type="button" class="goNextBtn PgBtn" value="<?php echo T("Next Page"); ?>">
+		</td>
+	</tr>
+	</tfoot>
+	</table>
+</div>
+
+<!-- ------------------------------------------------------------------------ -->
+<div id="editDiv">
+	<form role="form" id="editForm" name="editForm" >
+	<h5 class="note"> Fields marked with <span class="reqd">*</span> are required.</h5>
+	<fieldset>
+		<legend id="editHdr"></legend>
+		<table>
+		<tbody>
+			<!-- all input fields are constructed empty by server -->
+			<?php include(REL(__FILE__, "../circ/mbrFields.php")); ?>	
+		</tbody>	
+		<tfoot>
+			<tr>
+				<td colspan="2">
+					<input type="hidden" id="editMode" name="mode" value="">
+					<input type="hidden" id="mbrid" name="mbrid" value="">
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2" cl>
+					<input type="submit" id="addMbrBtn" value="<?php echo T("Add"); ?>" />
+					<input type="submit" id="updtMbrBtn" value="<?php echo T("Update"); ?>" />
+					<input type="button" class="gobkUpdtBtn" value="<?php echo T("Go Back"); ?>" />
+					<input type="button" class="gobkNewBtn" value="<?php echo T("Go Back"); ?>" />
+					<input type="button" id="deltMbrBtn" value="<?php echo T("Delete"); ?>" />
+				</td>
+			</tr>
+		</tfoot>
+	</table>
+	</fieldset>
+	</form>
+</div>
+
+<!-- ------------------------------------------------------------------------ -->
+<div id="biblioDiv">
+	<ul class="btnRow">
+		<li><input type="button" class="gobkBiblioBtn" value="<?php echo T("Go Back"); ?>" /></li>
+	</ul>
+	
+		<?php include(REL(__FILE__,"../catalog/itemDisplayForm.php")); ?>
+
+	<ul class="btnRow">
+		<li><input type="button" class="gobkBiblioBtn" value="<?php echo T("Go Back"); ?>"></li>
+	</ul>
+</div>
+
+<!-- ------------------------------------------------------------------------ -->
+<div id="copyEditorDiv">
+	<?php require_once(REL(__FILE__,"../catalog/copyEditorForm.php"));?>
+</div>
+
+<!-- ------------------------------------------------------------------------ -->
+<div id="acntDiv">
+	<input type="button" class="gobkAcntBtn" value="<?php echo T("Go Back"); ?>" />
+	<fieldset>
+		<legend><?php echo T("Add a Transaction"); ?></legend>
+		<form role="form" id="acntForm">
+			<label for="transaction_type_cd"><?php echo T("Transaction Type"); ?>:</label>
+			<select id="transaction_type_cd" name="transaction_type_cd"></select>
+			<br />
+			<label for="description"><?php echo T("Description"); ?>:</label>
+			<input type="text" required size="40" maxlength="128" id="description" name="description" />
+			<br />
+			<label for="amount"><?php echo T("Amount"); ?>:</label>
+			<input type="number" required size="12" id="amount" name="amount" 
+						 pattern="^\d+\.\d{2}$" 
+						 title="<?php echo T("A valid money amount");?>" />
+			<br />
+			<input type="hidden" name="mode" value="addAcntTrans" />
+			<input type="hidden" id="acntMbrid" name="mbrid" value="" />
+			<input type="submit" id="addTransBtn" value="<?php echo T("Add New"); ?>" />
+		</form>
+	</fieldset>
+	
+	<fieldset>
+		<legend><?php echo T("Transaction Activity"); ?></legend>
+		<table id="tranList">
+			<thead>
+			<tr>
+				<th>&nbsp</th>
+				<th><?php echo T("Date"); ?></th>
+				<th><?php echo T("Trans Type"); ?></th>
+				<th><?php echo T("Description"); ?></th>
+				<th><?php echo T("Amount"); ?></th>
+				<th><?php echo T("Balance"); ?></th>
+			</tr>
+			</thead>
+			<tbody class="striped"></tbody>
+		</table>
+	</fieldset>
+	<input type="button" class="gobkAcntBtn" value="<?php echo T("Go Back"); ?>" />
+</div>
+
+<!-- ------------------------------------------------------------------------ -->
+<div id="histDiv">
+	<input type="button" class="gobkHistBtn" value="<?php echo T("Go Back"); ?>" />
+	<fieldset>
+		<legend><?php echo T("Checkout History"); ?></legend>
+		<table id="histList">
+			<thead>
+			<tr>
+				<th><?php echo T("Item"); ?></th>
+				<th><?php echo T("In/Out"); ?></th>
+				<th><?php echo T("Date"); ?></th>
+			</tr>
+			</thead>
+			<tbody class="striped"></tbody>
+		</table>
+	</fieldset>
+	<input type="button" class="gobkHistBtn" value="<?php echo T("Go Back"); ?>" />
+</div>
+
+<!-- ------------------------------------------------------------------------ -->
+<?php
+  require_once(REL(__FILE__,'../shared/footer.php'));
+	
+	//include_once(REL(__FILE__,'./mbrEditorJs.php'));
+	include_once(REL(__FILE__,'../catalog/itemDisplayJs.php'));
+	include_once(REL(__FILE__,'../circ/memberJs.php'));
+?>	
+
+</body>
+</html>
