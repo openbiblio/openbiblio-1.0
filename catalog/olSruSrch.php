768f9b00445062f0861838d69e73c20497ffb035
Jane Sandberg <sandbej@linnbenton.edu>
2018-08-23 21:47:12 +0000

    Creating a new, very simple SRU search

--- catalog/olSruSrch.php
+++ catalog/olSruSrch.php
@@ -7,120 +7,19 @@
  * 'Dublin Core' (dc) query schema and the 'marcxml' record schema for response.
  */
 
-	$displayXML = FALSE;
 
-	$process = array();
-	$subfields = array();
-	$subfieldcount = 0;
-	$sruRcrdSchema = 'marcxml';
-//	$sruRcrdSchema = 'mods';
-echo "using SRU module<br />";
-	
- 	$qry ="operation=searchRetrieve"
- 			 ."&version=1.1"
-			 ."&query=$sQuery"
-		 	 ."&recordPacking=xml"
-			 ."&maximumRecords=$postVars['maxHits']"
-			 ."&recordSchema=$sruRcrdSchema"
-				;
-	//echo "query: $qry <br />";
-	//print_r($postVars);
-
-	#### send query to each host in turn and get response
-	$resp = array();
-	for($i = 0; $i < $postVars['numHosts']; $i++) {
-		//echo "host: ".$postVars[hosts][$i][host]."<br />";
-		if ($postVars['hosts'][$i]['service'] == 'Z3950') {
-			echo "<h3>Sorry! You have selected a Z3950 service HOST, but<br />"
-					."YAZ is not installed in your server's PHP interpreter.</h3>";
-			exit;
-		}
-		
-		$header = "GET /".$postVars['hosts'][$i]['db']." HTTP/1.1\r\n".
-					 		"HOST ".$postVars['hosts'][$i]['host']."\r\n".
-  					 	"Content-Type: application/x-www-form-urlencoded; charset=iso-8859-1\r\n".
-  					 	"Content-Length: ".strlen($qry)."\r\n\r\n";
-		//echo "header: $header <br />";
-
-		### establish a socket for this host
-		$theHost = $postVars['hosts'][$i]['host'];
-		$thePort = $postVars['hosts'][$i]['port'];
-		if(!isset($thePort) || empty($thePort)) $thePort = 7090;  // default port
-		//echo "url: '$theHost:$thePort'<br />timeout=$postVars['timeout']seconds<br />";
-		$fp = fsockopen($theHost, $thePort, $errNmbr, $errMsg, $postVars['timeout']);
-		$text = $header . $qry;
-		if(!$fp) {
-			echo "<p class=\"error\">you requested:</p>";
-			echo "<fieldset>".nl2br($text)."</fieldset>";
-			echo "<p class=\"error\">via socket on port $thePort. </p>";
-			echo "<h4>Please verify the correctness of your host URL and "
-					."that your server's firewall allows access to the web via the port specified.<br /><br />
-					If you have been sucessful recently, it is possible your internet connection 
-					is temporarily broken or slower than usual.<h4>";
-			//trigger_error("Socket error: $errMsg ($errNmbr)");
-			exit;
-		}
-
-		### send the query
-		fputs($fp, $text);
-echo "to host=>".nl2br($text)." at port "."$thePort<br />";
-		
-		### Added timeout on the stream itself (also in loop)- LJ
-		stream_set_timeout($fp, $postVars['timeout']);
-		$info = stream_get_meta_data($fp); 
-		
-		### fetch the response, if any
-//echo "preparing to read any responses <br />";
-		$hitList = '';
-    $headerDone = false;
-    while(!feof($fp) && (!$info['timed_out'])) {
-      $line = fgets($fp, 2048);
-//echo "line: <br />";print_r($line);echo "<br />";
-	  	$info = stream_get_meta_data($fp);
-//echo "info: <br />";print_r($info);echo "<br />";
-      if (!line) {
-      	echo "Failure while reading response from $theHost <br />";
-      	break;
-			} else if (strcmp($line, "\r\n") == 0) {
-echo "reading the header<br />";
-      	$headerDone = true;
-      } else if ($headerDone && (trim($line) != '')) {
-				$line = trim($line);
-//echo "collecting data<br />";
-echo "line: >>".$line."<<<br />";
-      	$hitList .= $line;
-      }
-      else {
-        // http header lines
-				//echo "$line <br />";
-			}
-  	}
-  	if (!empty($hitList)) $hits[$i] = $hitList;
-  	fclose($fp);
-echo "summary:<br />";print_r($hits[$i]);echo "<br />";
-	}
-	$ttlHits = 0;
-/*
-	### create and parse downloaded XML
-	$xml_parser = xml_parser_create();
-	for($i = 0; $i < $postVars['numHosts']; $i++) {
-	  if (!empty($hits[$i])) {
-			xml_parse_into_struct($xml_parser, $hits[$i], $hostRecords[$i]);
-
-			list($num_records, $marc[$i]) = get_marc_fields_from_xml($hostRecords[$i]);
-			$msg = $rslt[$i][0]['diagMsg'];
-			if (($num_Records == 0) && (!empty($msg))) {
-				echo "Host Diagnostic Response: $msg<br />\n";
-				echo "----<br />Details...<br />\n";
-				echo $qry."<br />\n";
-				print_r($rslt);echo "<br />\n";
-				exit;
-			}
-			//echo "host #$i rslt:<br />";print_r($rslt[$i]);echo "<br />---------<br />";
-			$ttlHits += $num_records;
-		}
-	}
-	xml_parser_free($xml_parser);
-*/
-
+$parser = xml_parser_create();
+$hits = array();
+$ttlHits = 0;
+$maxHits = 10;
+foreach ($postVars['hosts'] as $host) {
+        $query = '?version=1.1&operation=searchRetrieve&query='.urlencode($_POST['lookupVal']).'&startRecord=1&maximumRecords='.$maxHits.'5&recordSchema=marcxml';
+        $sruResults = file_get_contents('http://'.$host['host'].':'.$host['port'].'/'.$host['db'].$query);
+        $hits[] = $sruResults;
+        $xml = new SimpleXMLElement($sruResults);
+        $xml->registerXPathNamespace('srw', 'http://www.loc.gov/zing/srw/');
+        $numberHitsInHost = $xml->xpath('//srw:numberOfRecords/text()');
+        $ttlHits += (int)$numberHitsInHost;
+}
+xml_parser_free($parser);
 ?>
